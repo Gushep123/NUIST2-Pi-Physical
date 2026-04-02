24f417cee0409d2f90f23a07a6a8fb79f647d083
Gushep123 <207319913@qq.com>
2026-04-02 14:15:32 +0800

    useable version for Quiz

--- Quiz.py
+++ Quiz.py
@@ -1,43 +1,44 @@
+
 #Yang Letain 26/3/30 This is a program for quiz, Amimal quiz
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time 
 # NUIST Quiz Game in Python
 def quiz():
-    print("Welcome to the Animal Quiz!")
-    print("Answer the following questions:")
-    # Questions and Answers
-    questions = [
+	print("Welcome to the Animal Quiz!")
+	print("Answer the following questions:")
+	# Questions and Answers
+	questions = [
         "1. What is the largest animal on Earth?: a. Blue Whale, b. Mouse, c. Cat\n",
         "2. Which bird can fly backwards?: a. Cuckoo,b.Eagle, c.Hummingbird\n",
         "3. What is the only mammal capable of flight?: a. Bat, b. Squirrel, c.Dolphin\n"
-    ]
-    answers = [
+	]
+	answers = [
         "blue whale",
         "hummingbird",
         "bat"
-    ]
-    score = 0
-    # Ask questions
-    for i in range(len(questions)):
-        user_answer = input(questions[i]).strip().lower()
-        if user_answer == answers[i]:
-            print("Correct!")
-            score += 1
-	    GPIO.output(18,GPIO.HIGH)
-	    time.sleep(1)
-	    GPIO.output(18,GPIO.LOW)
-        else:
-            print("Incorrect!")
-	    GPIO.output(23,GPIO.HIGH)
-            time.sleep(1)
-            GPIO.output(23,GPIO.LOW)
-    # Provide final score
-    print("\nQuiz completed!")
-    print(f"You got {score}/{len(questions)} questions correct.")
+	]
+	score = 0
+	# Ask questions
+	for i in range(len(questions)):
+		user_answer = input(questions[i]).strip().lower()
+		if user_answer == answers[i]:
+			print("Correct!")
+			score += 1
+			GPIO.output(18,GPIO.HIGH)
+			time.sleep(1)
+			GPIO.output(18,GPIO.LOW)
+		else:
+			print("Incorrect!")
+			GPIO.output(17,GPIO.HIGH)
+			time.sleep(1)
+			GPIO.output(17,GPIO.LOW)
+			# Provide final score
+	print("\nQuiz completed!")
+	print(f"You got {score}/{len(questions)} questions correct.")
 
 # Run the quiz function
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
-GPIO.setup(23,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
 quiz()
